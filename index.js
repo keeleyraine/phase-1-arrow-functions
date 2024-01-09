2fe477e4c3249a5382ada70373490a339cceccd4
Keeley Madigan <keeleysprouts@gmail.com>
2024-01-08 21:25:33 -0800

    p1 arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (num1, num2) => num1 / num2;
+divide(2000,100);
+const square = parameter => parameter * parameter;
+square();
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add();
